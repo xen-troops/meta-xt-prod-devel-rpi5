3a1245d966c76b24615f9def065e56a01f26e838
Grygorii Strashko <grygorii_strashko@epam.com>
2024-06-04 12:59:02 +0300

    meta-xt-prod-devel-rpi5: add sample domu images generation
    Hence SD-card support is added the Zephyr guest domain images need to be
    present on SD-card, so add generation of sync and blinky images.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>

---
+++ get_bins.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/xen-troops/zephyr-dom0-xt/raw/rpi5_dom0_dev/prebuilt/zephyr_blinky.dtb -O zephyr_blinky.dtb
+wget https://github.com/xen-troops/zephyr-dom0-xt/raw/rpi5_dom0_dev/prebuilt/zephyr_sync.dtb -O zephyr_sync.dtb
+

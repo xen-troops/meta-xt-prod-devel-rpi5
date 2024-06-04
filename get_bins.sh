7fd5aa4774e039d212bdc1df0c51cb07a00fd3e2
Grygorii Strashko <grygorii_strashko@epam.com>
2024-06-04 13:05:04 +0300

    meta-xt-prod-devel-rpi5: add unikernel monkey sample image
    Add unikernel monkey sample image binary to Dom0 storage.
    
    Signed-off-by: Grygorii Strashko <grygorii_strashko@epam.com>

--- get_bins.sh
+++ get_bins.sh
@@ -2,4 +2,5 @@
 
 wget https://github.com/xen-troops/zephyr-dom0-xt/raw/rpi5_dom0_dev/prebuilt/zephyr_blinky.dtb -O zephyr_blinky.dtb
 wget https://github.com/xen-troops/zephyr-dom0-xt/raw/rpi5_dom0_dev/prebuilt/zephyr_sync.dtb -O zephyr_sync.dtb
+wget https://github.com/aoscloud/demo-services/raw/main/monkey_zephyr/src/helloworld_xen-arm64 -O helloworld_xen-arm64
 
